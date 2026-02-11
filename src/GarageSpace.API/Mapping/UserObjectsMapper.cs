0315eb62dad3accddd7322604d38ff7c2946a3fe
Vladislav.Taran <taran.soft@gmail.com>
2026-02-11 21:17:03 +0100

    send UserRegistered event message

---
+++ src/GarageSpace.API/Mapping/UserObjectsMapper.cs
@@ -0,0 +1,19 @@
+﻿using GarageSpaceAPI.Contracts.Dto;
+using GarageSpaceAPI.Contracts.Request;
+
+namespace GarageSpace.API.Mapping
+{
+    public class UserObjectsMapper : IUserObjectsMapper
+    {
+        public UserDto MapCreateUserRequestToUser(UserCreateRequest request) 
+        {
+            return new UserDto
+            {
+                Email = request.Email,
+                Name = request.Name,
+                Nickname = request.Nickname,
+                Password = request.Password
+            };
+        }
+    }
+}
